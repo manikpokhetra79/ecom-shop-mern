aee9371766be0ef21066c1e79c8d5e449aab0d77
Manik Gupta <manikpokhetra@gmail.com>
2021-10-26 22:50:04 +0530

    created directory structure

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
+function App() {
+  return (
+    <>
+      <Header />
+      <main>
+        <h1>Welcome to EcomShop</h1>;
+      </main>
+      <Footer />
+    </>
+  );
+}
+
+export default App;
